f172de3c2c37ba6dfe08d44a74df217a03e3f06f
himeshchalla <himeshchalla@gmail.com>
2017-09-09 21:38:00 +0530

    Added migration for password_resets table used for forgot password functionality

---
+++ database/migrations/2017_09_09_171227_create_password_resets_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePasswordResetsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('password_resets', function (Blueprint $table) {
+            $table->increments('id');
+        
+            $table->string('email')->index();
+            $table->string('token')->index();
+            $table->boolean('archived')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('password_resets', function (Blueprint $table) {
+            Schema::drop('password_resets');
+        });
+    }
+}
