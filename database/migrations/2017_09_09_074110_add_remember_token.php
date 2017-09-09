18ca82884667e7185c707748c852b92972c91079
himeshchalla <himeshchalla@gmail.com>
2017-09-09 18:32:37 +0530

    Added default/initial frontend login/signup/forget password/remember me functionality

---
+++ database/migrations/2017_09_09_074110_add_remember_token.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddRememberToken extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('cms_users', function (Blueprint $table) {
+            $table->string('remember_token', 256)->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('cms_users', function (Blueprint $table) {
+            $table->dropColumn('remember_token', 256);
+        });
+    }
+}
